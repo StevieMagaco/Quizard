57e8a41f8d3fd2cf1ee2a302cf7cd56ee0487501
Jackson Yelinek <jayelinek@fullsail.edu>
2016-08-15 22:17:53 -0400

    Merged the two projects into one solution, updated the watch where needed to make sure it will work, and updated gitignore as needed.

---
+++ QuizardPhone/QuizardWatch/DataStruct.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+
+namespace Quizard
+{
+    class DataStruct
+    {
+        public List<string> Answers { get; set; }
+        public List<string> Questions { get; set; }
+        public string NameOfSet { get; set; }
+        public int Count { get; set; }
+        public int Correnct { get; set; }
+        public int Incorrect { get; set; }
+    }
+}
