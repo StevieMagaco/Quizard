48a50512ab0559b206add30abc2498283364c0f6
Julien Gaupin <jdgaupin95@gmail.com>
2016-08-22 21:51:32 -0400

    bug fixes

--- QuizardPhone/Quizard/GeneralInterface/HomeActivity.cs
+++ QuizardPhone/Quizard/GeneralInterface/HomeActivity.cs
@@ -71,7 +71,7 @@ namespace Quizard
     {
         private ArrayAdapter mAdapter;
         private ListView mFlashSetList;
-        private EditText mFlashSetSubject;
+        private AutoCompleteTextView mFlashSetSubject;
         private ImageButton mCreateAFlashSet, mCancel, mEditAFlashSetsSubject, mDeleteAFlashSet, mSettings;
         private TextView mCreateAFlashSetLabel, mCancelLabel, mEditAFlashSetsSubjectLabel, mDeleteAFlashSetLabel, mSettingsLabel;
         private SearchView mSearchThroughFlashSets;
@@ -99,11 +99,39 @@ namespace Quizard
             // Set our view from the "HomeLayout" layout resource
             SetContentView(Resource.Layout.HomeLayout);
 
+            string[] autoCompleteOptions = new string[]
+            {
+                #region Subjects
+                "Language Arts",
+                "Mathematics",
+                "Science",
+                "Health",
+                "Physical Education",
+                "Art",
+                "Music",
+                "Speech",
+                "Reading",
+                "Debate",
+                "English",
+                "Basic Math",
+                "Pre-Algebra",
+                "Algebra",
+                "Geometry",
+                "Life Science",
+                "Earth Science",
+                "Physical Science",
+                "Social Studies",
+                "Geography",
+                "U.S. History",
+                "Literature",
+                #endregion
+            };
+
             mGoogleClient = new GoogleApiClient.Builder(this, this, this).AddApi(WearableClass.API).Build();
 
             #region Class Variable FindViewById<> Assignments
             mFlashSetList = FindViewById<ListView>(Resource.Id.flashSetListViewID);
-            mFlashSetSubject = FindViewById<EditText>(Resource.Id.flashSetSubjectEditTextID);
+            mFlashSetSubject = FindViewById<AutoCompleteTextView>(Resource.Id.flashSetSubjectAutoCompleteTextViewID);
             mCreateAFlashSet = FindViewById<ImageButton>(Resource.Id.createAFlashSetImageButtonID);
             mCancel = FindViewById<ImageButton>(Resource.Id.cancelImageButtonID);
             mEditAFlashSetsSubject = FindViewById<ImageButton>(Resource.Id.editFlashSetSubjectImageButtonID);
@@ -121,6 +149,9 @@ namespace Quizard
 
             mUserInformation = new DataBase.UserInfo();
             mAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, mSetNameList);
+
+            ArrayAdapter autoCompleteAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleDropDownItem1Line, autoCompleteOptions);
+            mFlashSetSubject.Adapter = autoCompleteAdapter;
 
             string Username_Buffer = Intent.GetStringExtra("UserName") ?? "Data not available";
 
@@ -164,7 +195,7 @@ namespace Quizard
             mFlashSetList.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs e)
             {
                 mSelectedFlashSet = e.Position;
-                mFlashSetSubject.Hint = "Edit " + ReturnRealSetName(mSetNameList[mSelectedFlashSet].ToString()) + "?";
+                mFlashSetSubject.Hint = "Edit \"" + ReturnRealSetName(mSetNameList[mSelectedFlashSet].ToString()) + "\"?";
 
                 mFlashSetSubject.Visibility = ViewStates.Visible;
                 mEnterIntoSelectedFlashSet.Visibility = ViewStates.Visible;
@@ -199,7 +230,7 @@ namespace Quizard
             mEnterIntoSelectedFlashSet.Click += delegate (object sender, EventArgs e)
             {
                 Intent intent = new Intent(this, typeof(DeckActivity));
-                string[] UserSetName = { mUserInformation.GetUser().GetUsername(),ReturnRealSetName( mSetNameList[mSelectedFlashSet].ToString()) };
+                string[] UserSetName = { mUserInformation.GetUser().GetUsername(), ReturnRealSetName(mSetNameList[mSelectedFlashSet].ToString()) };
                 intent.PutExtra("Username/SetName", UserSetName);
                 this.StartActivity(intent);
             };
@@ -220,6 +251,8 @@ namespace Quizard
                 mSettings.Visibility = ViewStates.Invisible;
                 mSettingsLabel.Visibility = ViewStates.Invisible;
                 #endregion
+
+                mFlashSetSubject.Hint = "Subject";
             };
 
             // If the user needs to cancel out of a flash set creation, update, or delete command...
@@ -275,7 +308,7 @@ namespace Quizard
             {
                 AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
-                alert.SetTitle("Delete the \"" + mFlashSetSubject.Text + "\" flash set?");
+                alert.SetTitle("Delete the \"" + ReturnRealSetName(mSetNameList[mSelectedFlashSet].ToString()) + "\" flash set?");
 
                 alert.SetPositiveButton("Yes", (senderAlert, args) =>
                 {
@@ -411,7 +444,7 @@ namespace Quizard
                 while (SetInfo.MoveToNext())
                 {
                     string name = SetInfo.GetString(1);
-                   int CardCount = GetCardCount(mUserInformation.GetUser().GetUsername(), name);
+                    int CardCount = GetCardCount(mUserInformation.GetUser().GetUsername(), name);
                     mSetNameList.Add(name + "\n" + CardCount.ToString());
                     // mSetNameList.Add(name);
                     BufferSet = new DataBase.Sets();
@@ -445,7 +478,7 @@ namespace Quizard
                     DataBase.DBAdapter db = new DataBase.DBAdapter(this);
                     db.openDB();
 
-                    ICursor CardsInfo = db.GetCards(mUserInformation.GetUser().GetUsername(),ReturnRealSetName( mSetNameList[loop]));
+                    ICursor CardsInfo = db.GetCards(mUserInformation.GetUser().GetUsername(), ReturnRealSetName(mSetNameList[loop]));
                     DataBase.Cards BufferCard = new DataBase.Cards();
                     while (CardsInfo.MoveToNext())
                     {
@@ -512,7 +545,7 @@ namespace Quizard
         {
             DataBase.DBAdapter db = new DataBase.DBAdapter(this);
             db.openDB();
-           _SetName = ReturnRealSetName(_SetName);
+            _SetName = ReturnRealSetName(_SetName);
             if (db.DeleteRowSet_tb(_Username, _SetName))
             {
                 RetreiveSet(mFlashSetList, _Username);
@@ -584,7 +617,7 @@ namespace Quizard
         public string ReturnRealSetName(string _Setname)
         {
             string SetNameBuffer = "";
-            for(int loop = 0; loop < _Setname.Length; loop++)
+            for (int loop = 0; loop < _Setname.Length; loop++)
             {
                 if (_Setname[loop] != '\n')
                 {
@@ -601,7 +634,7 @@ namespace Quizard
             DataBase.Cards CardBuffer = new DataBase.Cards();
             db.openDB();
             ICursor Cards = db.GetCards(_Username, _Setname);
-            while(Cards.MoveToNext())
+            while (Cards.MoveToNext())
             {
                 CardBuffer.SetUserName(_Username);
                 CardBuffer.SetSetName(_Setname);
